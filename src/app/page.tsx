c89fd115ccfa3a8955574c58cec4b4683ab9c021
Rummonislam786 <islam.rummon786@gmail.com>
2025-05-29 16:38:16 +0600

    Responsive ness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,29 +5,26 @@ import { FaShapes } from "react-icons/fa6";
 
 export default function Home() {
   return (
-   <div className="min-h-screen bg-gray-50 py-12 px-4">
-
-      <div className="max-w-4xl mx-auto">
-        
-        <div className="bg-white p-8 shadow-sm rounded-lg border flex flex-col w-full">
-          
-          <div className="mb-8">
-            <h1 className="text-2xl font-semibold text-gray-900 mb-6">
+   <div className="min-h-screen bg-gray-50 py-8 px-2 sm:py-12 sm:px-4">
+      <div className="max-w-4xl mx-auto w-full">
+        <div className="bg-white p-4 sm:p-8 shadow-sm rounded-lg border flex flex-col w-full">
+          <div className="mb-6 sm:mb-8">
+            <h1 className="text-xl sm:text-2xl font-semibold text-gray-900 mb-4 sm:mb-6">
               Create a store
             </h1>
-            <p className="text-black space-x-3 ">
+            <p className="text-black space-x-1 sm:space-x-3 text-sm sm:text-base">
               Add your basic store information and complete the setup
             </p>
             <hr className="my-2 border-gray-300"/>
           </div>
-          <div className="space-y-6 mb-3">
-            <div className="flex items-center space-x-2">
+          <div className="space-y-4 sm:space-y-6 mb-3">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center sm:space-x-2 space-y-2 sm:space-y-0">
               <div className="self-start pt-1">
-              <Monitor className="text-blue-500 w-5 h-5"/>
+                <Monitor className="text-blue-500 w-5 h-5"/>
               </div>
-              <div className="flex-1 space-y-4">
+              <div className="flex-1 space-y-2 sm:space-y-4">
                 <div>
-                  <h3 className="text-sm font-small text-gray-900 mb-1 font-bold">
+                  <h3 className="text-sm sm:text-base font-small text-gray-900 mb-1 font-bold">
                     Give your online store a name
                   </h3>
                   <p className="text-xs text-gray-600 leading-relaxed">
@@ -35,17 +32,17 @@ export default function Home() {
                   </p>
                 </div>
               </div>
-              <input className="w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
+              <input className="w-full sm:w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
             </div>
           </div>
-          <div className="space-y-6 mb-3">
-          <div className="flex items-center space-x-2">
+          <div className="space-y-4 sm:space-y-6 mb-3">
+          <div className="flex flex-col sm:flex-row items-start sm:items-center sm:space-x-2 space-y-2 sm:space-y-0">
               <div className="self-start pt-1">
-              <Earth className="text-blue-500 w-5 h-5"/>
+                <Earth className="text-blue-500 w-5 h-5"/>
               </div>
-              <div className="flex-1 space-y-4 gap-4">
+              <div className="flex-1 space-y-2 sm:space-y-4 gap-4">
                 <div>
-                  <h3 className="text-sm font-small text-gray-900 mb-1 font-bold">
+                  <h3 className="text-sm sm:text-base font-small text-gray-900 mb-1 font-bold">
                     Give your online store a name
                   </h3>
                   <p className="text-xs text-gray-600 leading-relaxed">
@@ -53,18 +50,17 @@ export default function Home() {
                   </p>
                 </div>
               </div>
-              <input className="w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
+              <input className="w-full sm:w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
             </div>
           </div>
-          <div className="space-y-6 mb-3">
-            <div className="flex items-center space-x-2">
+          <div className="space-y-4 sm:space-y-6 mb-3">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center sm:space-x-2 space-y-2 sm:space-y-0">
               <div className="self-start pt-1">
-              <MdOutlineEditLocation className="text-blue-500 w-5 h-5" />
+                <MdOutlineEditLocation className="text-blue-500 w-5 h-5" />
               </div>
-              
-              <div className="flex-1 space-y-4 gap-4">
+              <div className="flex-1 space-y-2 sm:space-y-4 gap-4">
                 <div>
-                  <h3 className="text-sm font-small text-gray-900 mb-1 font-bold">
+                  <h3 className="text-sm sm:text-base font-small text-gray-900 mb-1 font-bold">
                     Give your online store a name
                   </h3>
                   <p className="text-xs text-gray-600 leading-relaxed">
@@ -72,17 +68,35 @@ export default function Home() {
                   </p>
                 </div>
               </div>
-              <input className="w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
+              <input className="w-full sm:w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
             </div>
           </div>
-          <div className="space-y-6 mb-3">
-            <div className="flex items-center space-x-2">
+          <div className="space-y-4 sm:space-y-6 mb-3">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center sm:space-x-2 space-y-2 sm:space-y-0">
               <div className="self-start pt-1">
-              <FaShapes className="text-blue-500 w-4 h-4" />
+                <FaShapes className="text-blue-500 w-4 h-4" />
               </div>
-              <div className="flex-1 space-y-4 gap-4">
+              <div className="flex-1 space-y-2 sm:space-y-4 gap-4">
                 <div>
-                  <h3 className="text-sm font-small text-gray-900 mb-1 font-bold">
+                  <h3 className="text-sm sm:text-base font-small text-gray-900 mb-1 font-bold">
+                    Give your online store a name
+                  </h3>
+                  <p className="text-xs sm:text-sm text-gray-600 leading-relaxed">
+                    A great store name is a big part of your success. Make sure it aligns with your brand and products.
+                  </p>
+                </div>
+              </div>
+              <input className="w-full sm:w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
+            </div>
+          </div>
+          <div className="space-y-4 sm:space-y-6 mb-3">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center sm:space-x-2 space-y-2 sm:space-y-0">
+              <div className="self-start pt-1">
+                <MdCurrencyExchange className="text-blue-500 w-4 h-4" />
+              </div>
+              <div className="flex-1 space-y-2 sm:space-y-4 gap-4">
+                <div>
+                  <h3 className="text-sm sm:text-base font-small text-gray-900 mb-1 font-bold">
                     Give your online store a name
                   </h3>
                   <p className="text-xs text-gray-600 leading-relaxed">
@@ -90,17 +104,17 @@ export default function Home() {
                   </p>
                 </div>
               </div>
-              <input className="w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
+              <input className="w-full sm:w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
             </div>
           </div>
-          <div className="space-y-6 mb-3">
-            <div className="flex items-center space-x-2">
+          <div className="space-y-4 sm:space-y-6 mb-3">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center sm:space-x-2 space-y-2 sm:space-y-0">
               <div className="self-start pt-1">
-              <MdCurrencyExchange className="text-blue-500 w-4 h-4" />
+                <Mail className="text-blue-500 w-4 h-4" />
               </div>
-              <div className="flex-1 space-y-4 gap-4">
+              <div className="flex-1 space-y-2 sm:space-y-4 gap-4">
                 <div>
-                  <h3 className="text-sm font-small text-gray-900 mb-1 font-bold">
+                  <h3 className="text-sm sm:text-base font-small text-gray-900 mb-1 font-bold">
                     Give your online store a name
                   </h3>
                   <p className="text-xs text-gray-600 leading-relaxed">
@@ -108,28 +122,10 @@ export default function Home() {
                   </p>
                 </div>
               </div>
-              <input className="w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
+              <input className="w-full sm:w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
             </div>
           </div>
-          <div className="space-y-6 mb-3">
-            <div className="flex items-center space-x-2">
-              <div className="self-start pt-1">
-              <Mail className="text-blue-500 w-4 h-4" />
-              </div>
-              <div className="flex-1 space-y-4 gap-4">
-                <div>
-                  <h3 className="text-sm font-small text-gray-900 mb-1 font-bold">
-                    Give your online store a name
-                  </h3>
-                  <p className="text-xs text-gray-600 leading-relaxed">
-                    A great store name is a big part of your success. Make sure it aligns with your brand and products.
-                  </p>
-                </div>
-              </div>
-              <input className="w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
-            </div>
-          </div>
-          <button className="w-1/5 bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition-colors duration-200 self-end">Create Store</button>
+          <button className="w-full sm:w-1/3 md:w-1/5 bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition-colors duration-200 self-end mt-4">Create Store</button>
         </div>
       </div>
     </div>
