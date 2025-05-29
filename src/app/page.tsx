4f3c9f878bf76e45fe4af34dac5ffd4dff81c16b
Rummonislam786 <islam.rummon786@gmail.com>
2025-05-29 09:43:08 +0600

    created the layout for 1st task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,137 @@
-import Image from "next/image";
+import { Monitor,Earth,Mail } from "lucide-react";
+import { MdCurrencyExchange } from "react-icons/md";
+import { MdOutlineEditLocation } from "react-icons/md";
+import { FaShapes } from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+   <div className="min-h-screen bg-gray-50 py-12 px-4">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="max-w-4xl mx-auto">
+        
+        <div className="bg-white p-8 shadow-sm rounded-lg border flex flex-col w-full">
+          
+          <div className="mb-8">
+            <h1 className="text-2xl font-semibold text-gray-900 mb-6">
+              Create a store
+            </h1>
+            <p className="text-black space-x-3 ">
+              Add your basic store information and complete the setup
+            </p>
+            <hr className="my-2 border-gray-300"/>
+          </div>
+          <div className="space-y-6 mb-3">
+            <div className="flex items-center space-x-2">
+              <div className="self-start pt-1">
+              <Monitor className="text-blue-500 w-5 h-5"/>
+              </div>
+              <div className="flex-1 space-y-4">
+                <div>
+                  <h3 className="text-sm font-small text-gray-900 mb-1 font-bold">
+                    Give your online store a name
+                  </h3>
+                  <p className="text-xs text-gray-600 leading-relaxed">
+                    A great store name is a big part of your success. Make sure it aligns with your brand and products.
+                  </p>
+                </div>
+              </div>
+              <input className="w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
+            </div>
+          </div>
+          <div className="space-y-6 mb-3">
+          <div className="flex items-center space-x-2">
+              <div className="self-start pt-1">
+              <Earth className="text-blue-500 w-5 h-5"/>
+              </div>
+              <div className="flex-1 space-y-4 gap-4">
+                <div>
+                  <h3 className="text-sm font-small text-gray-900 mb-1 font-bold">
+                    Give your online store a name
+                  </h3>
+                  <p className="text-xs text-gray-600 leading-relaxed">
+                    A great store name is a big part of your success. Make sure it aligns with your brand and products.
+                  </p>
+                </div>
+              </div>
+              <input className="w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
+            </div>
+          </div>
+          <div className="space-y-6 mb-3">
+            <div className="flex items-center space-x-2">
+              <div className="self-start pt-1">
+              <MdOutlineEditLocation className="text-blue-500 w-5 h-5" />
+              </div>
+              
+              <div className="flex-1 space-y-4 gap-4">
+                <div>
+                  <h3 className="text-sm font-small text-gray-900 mb-1 font-bold">
+                    Give your online store a name
+                  </h3>
+                  <p className="text-xs text-gray-600 leading-relaxed">
+                    A great store name is a big part of your success. Make sure it aligns with your brand and products.
+                  </p>
+                </div>
+              </div>
+              <input className="w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
+            </div>
+          </div>
+          <div className="space-y-6 mb-3">
+            <div className="flex items-center space-x-2">
+              <div className="self-start pt-1">
+              <FaShapes className="text-blue-500 w-4 h-4" />
+              </div>
+              <div className="flex-1 space-y-4 gap-4">
+                <div>
+                  <h3 className="text-sm font-small text-gray-900 mb-1 font-bold">
+                    Give your online store a name
+                  </h3>
+                  <p className="text-xs text-gray-600 leading-relaxed">
+                    A great store name is a big part of your success. Make sure it aligns with your brand and products.
+                  </p>
+                </div>
+              </div>
+              <input className="w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
+            </div>
+          </div>
+          <div className="space-y-6 mb-3">
+            <div className="flex items-center space-x-2">
+              <div className="self-start pt-1">
+              <MdCurrencyExchange className="text-blue-500 w-4 h-4" />
+              </div>
+              <div className="flex-1 space-y-4 gap-4">
+                <div>
+                  <h3 className="text-sm font-small text-gray-900 mb-1 font-bold">
+                    Give your online store a name
+                  </h3>
+                  <p className="text-xs text-gray-600 leading-relaxed">
+                    A great store name is a big part of your success. Make sure it aligns with your brand and products.
+                  </p>
+                </div>
+              </div>
+              <input className="w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
+            </div>
+          </div>
+          <div className="space-y-6 mb-3">
+            <div className="flex items-center space-x-2">
+              <div className="self-start pt-1">
+              <Mail className="text-blue-500 w-4 h-4" />
+              </div>
+              <div className="flex-1 space-y-4 gap-4">
+                <div>
+                  <h3 className="text-sm font-small text-gray-900 mb-1 font-bold">
+                    Give your online store a name
+                  </h3>
+                  <p className="text-xs text-gray-600 leading-relaxed">
+                    A great store name is a big part of your success. Make sure it aligns with your brand and products.
+                  </p>
+                </div>
+              </div>
+              <input className="w-1/2 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-400"/>
+            </div>
+          </div>
+          <button className="w-1/5 bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition-colors duration-200 self-end">Create Store</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
